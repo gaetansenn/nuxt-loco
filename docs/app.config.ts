09fa5fe1e9ce52a8df4697fe6d7d79c7e624e31d
Gaetan SENN <gaetan.senn@gmail.com>
2023-01-09 10:47:48 +0100

    feat: restore docs

---
+++ docs/app.config.ts
@@ -0,0 +1,17 @@
+export default defineAppConfig({
+  docus: {
+    title: 'Nuxt-loco',
+    description: 'A nuxt module to sync your translations from localise.biz',
+    socials: {
+      twitter: '@gaetansenn',
+      github: 'gaetansenn/nuxt-loco'
+    },
+    aside: {
+      level: 0,
+      exclude: []
+    },
+    header: {
+      exclude: []
+    }
+  }
+})
